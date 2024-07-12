2790702a2bed8356770ae7aa920569e72bb8a66d
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-12 01:40:41 -0300

    2.5.9 commit

--- challenge-02/Assets/Challenge 2/Scripts/MoveForwardX.cs
+++ challenge-02/Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -7,7 +7,7 @@ public class MoveForwardX : MonoBehaviour
     public float speed;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
--- challenge-02/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ challenge-02/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,23 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+        public float coolDown;
+    public const float coolDownTime = 0.5F;
+
     // Update is called once per frame
     void Update()
     {
+        if(coolDown > 0){
+            coolDown -= Time.deltaTime;
+        }
+
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if(coolDown <= 0){
+                coolDown = coolDownTime;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
         }
     }
 }
