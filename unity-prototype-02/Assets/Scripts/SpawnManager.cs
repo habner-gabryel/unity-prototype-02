cc66597309cb8d7dc79ce7513093564ecf0ccff2
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-11 23:40:43 -0300

    2.3.2 commit

--- unity-prototype-02/Assets/Scripts/SpawnManager.cs
+++ unity-prototype-02/Assets/Scripts/SpawnManager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[ ] animalPrefabs; 
-    
+    public GameObject[ ] animalPrefabs;
+    public int animalIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,8 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.S)){
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0,0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
